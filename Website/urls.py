f13244bcb9da042668a2f0842370a0b3cf270471
rajgopalhota <rajgopalhota.com>
2023-06-20 09:28:20 +0530

    tabs initiated

--- Website/urls.py
+++ Website/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('marksheets', views.marksheets, name='marksheets'),
+    path('timetable', views.timetable, name='timetable'),
+    path('announcements', views.announcements, name='announcements'),
+    path('reviews', views.reviews, name='reviews'),
     
 ]
 
--- Website/views.py
+++ Website/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'components/home.html')
+
+def reviews(request):
+    return render(request, 'components/reviews.html')
+
+def announcements(request):
+    return render(request, 'components/announcements.html')
+
+def timetable(request):
+    return render(request, 'components/timetable.html')
+
+def marksheets(request):
+    return render(request, 'components/marksheets.html')
