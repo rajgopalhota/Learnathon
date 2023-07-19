ebda92e7683fd78fc690b8dfe2a07d14eeaf5646
rajgopalhota <rajgopalhota@gmail.com>
2023-07-19 10:55:31 +0530

    Authentication extra

--- Website/views.py
+++ Website/views.py
@@ -17,7 +17,7 @@ def home(request):
     return render(request, 'components/home.html')
 
 def reviews(request):
-    if request.method == "POST":
+    if request.method == "POST" and request.user.is_authenticated:
         teamno = request.POST.get('teamno')
         idno = request.POST.get('idno')
         roomno = request.POST.get('roomno')
@@ -26,6 +26,9 @@ def reviews(request):
         complaint = Complaints(teamno=teamno, idno=idno, roomno = roomno, query = query, msg = msg)
         complaint.save()
         messages.success(request, "Your Complaint has been posted successfully")
+    else:
+        messages.warning(request,"please login first and add the complaint then")
+        return redirect('login')
     return render(request, 'components/reviews.html')
 
 def announcements(request):
@@ -38,20 +41,25 @@ def timetable(request):
     context = {'timetable': tt}
     return render(request, 'components/timetable.html', context)
 
+
 def marksheets(request):
     # print(request.user)
     # print(Student.objects.get(user=request.user))
-    a=Student.objects.filter(user=User.objects.get(username=request.user))
-    x=0
-    for i in a:
-        x=i
-        break
-    print(x.name)
-    context={}
-    context['student']=x
-    context['marks']=Review.objects.filter(student=x).order_by('review_no')
+    if request.user.is_authenticated:
+        a=Student.objects.filter(user=User.objects.get(username=request.user))
+        x=0
+        for i in a:
+            x=i
+            break
+        print(x.name)
+        context={}
+        context['student']=x
+        context['marks']=Review.objects.filter(student=x).order_by('review_no')
         
-    return render(request, 'components/marksheets.html',context)
+        return render(request, 'components/marksheets.html',context)
+    else:
+        messages.warning(request,"login first to see the marksheets")
+        return redirect('login')
 
 def leaderboard(request):
     a=Student.objects.all()
@@ -76,7 +84,7 @@ def leaderboard(request):
 # Main typo..............
 def signin(request):
     # lg = login.objects.all().values()
-    if request.method == 'POST':
+    if request.method == 'POST' :
         username = request.POST.get('uname')
         pass1 = request.POST.get('pass')
         user = authenticate(username=username, password=pass1)
@@ -101,116 +109,159 @@ def signout(request):
 # Faculty
 
 def room_display(request):
+    # if request.user.is_authenticated and request.user.is_staff:
     context={}
     context["rooms"]=Room.objects.all()
     context["sessions"]=Session.objects.all()
     return render(request,"components/rooms.html",context)
-
+    # else:
+        # messages.error(request,"you does not have acess to use this")
+        # logout(request)
+        # return redirect('home')
 
 def handleroom(request):
-    a=Room.objects.get(room_no=(request.POST["room"]))
-    b=Student.objects.filter(room=a)
-    context={}
-    context["room"]=a
-    context["students"]=b
-    session=Session.objects.get(session_no=int(request.POST['session']))
+    if request.user.is_staff and request.user.is_authenticated:
+        a=Room.objects.get(room_no=(request.POST["room"]))
+        b=Student.objects.filter(room=a)
+        context={}
+        context["room"]=a
+        context["students"]=b
+        session=Session.objects.get(session_no=int(request.POST['session']))
     
-    if session.Attendence == True:
-        context["session"]=session
-        return redirect('postattendance',room_no=str(a),session_no=request.POST['session'])
+        if session.Attendence == True:
+            context["session"]=session
+            return redirect('postattendance',room_no=str(a),session_no=request.POST['session'])
+        else:
+            return HttpResponse("Attendence not opened")
     else:
-        return HttpResponse("Attendence not opened")
+        messages.error(request,"you don't have acess to use this")
+        logout(request)
+        return redirect("home")
+    
 
 
 def postattendance(request, room_no, session_no):
-    room = Room.objects.get(room_no=room_no)
-    session = Session.objects.get(session_no=session_no, Attendence=True)
+    if request.user.is_staff and request.user.is_authenticated:
+        room = Room.objects.get(room_no=room_no)
+        session = Session.objects.get(session_no=session_no, Attendence=True)
 
-    if request.method == 'POST':
-        form = AttendanceForm(request.POST, session_id=session_no, room_no=room_no)
-        if form.is_valid():
-            form.save_attendance(session)
-            return HttpResponse('attendance_success')  # Redirect to a success page
+        if request.method == 'POST' and request.user.is_staff:
+            form = AttendanceForm(request.POST, session_id=session_no, room_no=room_no)
+            if form.is_valid():
+                form.save_attendance(session)
+                return HttpResponse('attendance_success')  # Redirect to a success page
+        elif not request.user.is_staff:
+            return HttpResponse("you are prohibeted to do this")
+        else:
+            form = AttendanceForm(session_id=session.session_no, room_no=room_no)
+    
+        return render(request, 'components/postattendance.html', {'form': form})
     else:
-        form = AttendanceForm(session_id=session.session_no, room_no=room_no)
-
-    return render(request, 'components/postattendance.html', {'form': form})
+        messages.error(request,"you don't have acess to use this")
+        logout(request)
+        return redirect("home")
+    
 
 
 
 def room_display_review(request):
-    context={}
-    context["rooms"]=Room.objects.all()
-    context["sessions"]=Session.objects.all()
-    return render(request,"components/rooms_review.html",context)
-
+    if request.user.is_staff and request.user.is_authenticated:
+        context={}
+        context["rooms"]=Room.objects.all()
+        context["sessions"]=Session.objects.all()
+        return render(request,"components/rooms_review.html",context)
+    else:
+        messages.error(request,"you did not have acess to use this")
+        logout(request)
+        return redirect("home")
+    
 
 def handle_review_room(request):
-    a=Room.objects.get(room_no=(request.POST["room"]))
-    b=Student.objects.filter(room=a)
-    context={}
-    context["room"]=a
-    context["students"]=b
-    context["teams"]=Team.objects.filter(room=a)
-    session=Session.objects.get(session_no=int(request.POST['session']))
+    if request.user.is_staff and request.user.is_authenticated:
+        a=Room.objects.get(room_no=(request.POST["room"]))
+        b=Student.objects.filter(room=a)
+        context={}
+        context["room"]=a
+        context["students"]=b
+        context["teams"]=Team.objects.filter(room=a)
+        session=Session.objects.get(session_no=int(request.POST['session']))
     
-    if session.Attendence == True:
-        context["session"]=session
-        return render(request,'components/teams.html',context)
+        if session.Attendence == True:
+            context["session"]=session
+            return render(request,'components/teams.html',context)
+        else:
+            return HttpResponse("session not activated")
+
     else:
-        return HttpResponse("session not activated")
+        messages.error(request,"you does not have the permissions to use this")
+        logout(request)
+        return redirect("home")
 
 
 def handle(request,room,session):
-    a=Room.objects.get(room_no=room)
-    b=Student.objects.filter(room=a)
-    context={}
-    context["room"]=a
-    context["students"]=b
-    context["teams"]=Team.objects.filter(room=a)
-    session=Session.objects.get(session_no=int(session))
+    if request.user.is_staff and request.user.is_authenticated:
+        a=Room.objects.get(room_no=room)
+        b=Student.objects.filter(room=a)
+        context={}
+        context["room"]=a
+        context["students"]=b
+        context["teams"]=Team.objects.filter(room=a)
+        sessio=Session.objects.get(session_no=session)
     
-    if session.Attendence == True:
-        context["session"]=session
-        return render(request,'components/teams.html',context)
+        if sessio.Attendence == True:
+            context["session"]=sessio
+            return render(request,'components/teams.html',context)
+        else:
+            return HttpResponse("session not activated")
     else:
-        messages.error(request,"session not yet opened")
-        return render(request,'components/teams.html')
+        messages.error(request,"you does not have the permissions to use this")
+        logout(request)
+        return redirect("home")
 
 
 def handle_team(request, session_no, room_no):
-    team_id = request.POST.get('team')
-    redirect_url = reverse('postmarks', args=(session_no, room_no, team_id))
-    return redirect(redirect_url)
+    if request.user.is_authenticated and request.user.is_staff:
+        team_id = request.POST.get('team')
+        redirect_url = reverse('postmarks', args=(session_no, room_no, team_id))
+        return redirect(redirect_url)
+    else:
+        messages.error(request,"you don't have the acess to use this")
+        logout(request)
+        return redirect("home")
 
 def postmarks(request, session_no, room_no, team_name):
-    r = Room.objects.get(room_no=room_no)
-    session = Session.objects.get(session_no=session_no, Attendence=True)
-    team = Team.objects.get(team_name=team_name)
-    a = Student.objects.filter(team=team)
-    context = {}
-    context["students"] = a
+    if request.user.is_staff and request.user.is_authenticated:
+        r = Room.objects.get(room_no=room_no)
+        session = Session.objects.get(session_no=session_no, Attendence=True)
+        team = Team.objects.get(team_name=team_name)
+        a = Student.objects.filter(team=team)
+        context = {}
+        context["students"] = a
 
-    if request.method == 'POST':
-        for i in a:
-            review_marks = request.POST[str(i.id)]
-            if Review.objects.filter(student=i, session=session, review_no=session_no, room=r).exists():
-                messages.error(request,"updation of the marks is not possible")
+        if request.method == 'POST':
+            for i in a:
+                review_marks = request.POST[str(i.id)]
+                if Review.objects.filter(student=i, session=session, review_no=session_no, room=r).exists():
+                    messages.error(request,"updation of the marks is not possible")
                 # return redirect("/handle" + "/" + room_no + "/" + session_no)
-                return redirect('handle',room=room_no,session=session_no)
+                    return redirect('handle',room=room_no,session=session_no)
                 # return HttpResponse('marks already posted cannot modify the marks')
-            Review.objects.update_or_create(
-                student=i,
-                session=session,
-                review_no=session.session_no,
-                room=r,
-                defaults={'review_marks': review_marks}
-            )
+                Review.objects.update_or_create(
+                    student=i,
+                    session=session,
+                    review_no=session.session_no,
+                    room=r,
+                    defaults={'review_marks': review_marks}
+                )
         # team.submitted = True
         # team.save()
-        return redirect('handle',room=room_no,session=session_no)
+            return redirect('handle',room=room_no,session=session_no)
 
-    return render(request, 'components/postmarks.html', context=context)
+        return render(request, 'components/postmarks.html', context=context)
+    else:
+        messages.error(request,"you don't have acess to use this")
+        return redirect('home')
+
 def errorPage(request, exception):
     # we add the path to the 404.html file
     # here. The name of our HTML file is 404.html
@@ -221,13 +272,17 @@ def error500Page(request, exception=None, *_, **_k):
     return render(request, 'components/error.html')
 
 def viewattendance(request):
-    a=Student.objects.filter(user=User.objects.get(username=request.user))
-    x=0
-    for i in a:
-        x=i
-        break
-    print(x.name)
-    context={}
-    context['student']=x
-    context['att']=Attendence.objects.filter(student=x).order_by('session')
-    return render(request,'components/viewattendance.html',context)
+    if request.user.is_authenticated :
+        a=Student.objects.filter(user=User.objects.get(username=request.user))
+        x=0
+        for i in a:
+            x=i
+            break
+        print(x.name)
+        context={}
+        context['student']=x
+        context['att']=Attendence.objects.filter(student=x).order_by('session')
+        return render(request,'components/viewattendance.html',context)
+    else:
+        messages.error(request,"login first to view this")
+        return redirect("login")
